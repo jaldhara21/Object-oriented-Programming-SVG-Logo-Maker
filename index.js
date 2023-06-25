f4e0063055616918c2ec32de2d95cedb8d91dd60
jaldhara.patel <jaldharapatel91@gmail.com>
2023-06-25 09:01:31 -0400

    added code for usertest and usershape for logo

--- index.js
+++ index.js
@@ -45,3 +45,62 @@ const questions = [
     message: "enter a color keyword (OR a hexadecimal number): ",
   },
 ];
+// Function to write data to file
+function writeToFile(fileName, data) {
+  console.log(`Writing [${data}] to file [${fileName}]`);
+  filesystem.writeFile(fileName, data, function (err) {
+    if (err) {
+      return console.log(err);
+    }
+    console.log("Congratulations, you have generated a logo.svg!");
+  });
+}
+async function init() {
+  console.log("Starting init");
+
+  // Prompt the user for answers
+  const answers = await inquirer.prompt(questions);
+
+  const userText = answers.text;
+  if (userText.length === 0 || userText.length > 3) {
+    console.log("Invalid user text field detected! Please enter 1-3 characters.");
+    return;
+  }
+  console.log(`User text: [${userText}]`);
+
+  const userFontColor = answers["text-color"];
+  console.log(`User font color: [${userFontColor}]`);
+
+  const userShapeColor = answers["shape-color"];
+  console.log(`User shape color: [${userShapeColor}]`);
+
+  const userShapeType = answers.shape;
+  console.log(`User entered shape: [${userShapeType}]`);
+
+  let userShape;
+  if (userShapeType === "Square" || userShapeType === "square") {
+    userShape = new Square();
+    console.log("User selected Square shape");
+  } else if (userShapeType === "Circle" || userShapeType === "circle") {
+    userShape = new Circle();
+    console.log("User selected Circle shape");
+  } else if (userShapeType === "Triangle" || userShapeType === "triangle") {
+    userShape = new Triangle();
+    console.log("User selected Triangle shape");
+  } else {
+    console.log("Invalid shape!");
+  }
+  userShape.setColor(userShapeColor);
+
+  const svg = new Svg();
+  svg.setTextElement(userText, userFontColor);
+  svg.setShapeElement(userShape);
+  const svgString = svg.render();
+
+  console.log("Displaying shape:\n\n" + svgString);
+  console.log("Shape generation complete!");
+  console.log("Writing shape to file...");
+  writeToFile("logo.svg", svgString);
+}
+
+init();
